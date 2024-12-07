a0d144c4a23b52801f401063f690d85d9d419fe3
muhammadTaha02 <tahatariq10000@gmail.com>
2024-12-07 09:09:39 +0500

    My second assigment

--- assignment01.dart
+++ assignment01.dart
@@ -1,3 +1,11 @@
 void main() {
-  print("Git ");
+  //Q.no1
+  num length = 10;
+  num breadth = 15;
+
+  if (length == breadth) {
+    print("square");
+  } else {
+    print("rectangular");
+  }
 }
