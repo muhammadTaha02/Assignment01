3581e018b00d75a022fefb7c86a6eaa41b5b5188
muhammadTaha02 <tahatariq10000@gmail.com>
2024-12-09 00:41:52 +0500

    first Assignment

--- assignment02.dart
+++ assignment02.dart
@@ -2,13 +2,13 @@ void main(){
 
 //Qno.2
 String name1 = "Muhammad Taha";
-int rizwan_age = "42";
+int rizwan_age = 42;
 String name2 ="Muhammad Hamza";
-int asad_age = "38";
+int asad_age = 38;
 
 if (rizwan_age <= asad_age){
     print ("$name1 is younger then $name2");
-}else if {
+}else {
     print ("$name2 is younger then $name1");
 }
 
---
+++ assignment03.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Qno.03
+  int num_classes = 16;
+  int att_classes = 10;
+  if (att_classes / num_classes * 100 >= 75) {
+    print("Allowed in exam");
+  } else {
+    print("not allowed");
+  }
+}
